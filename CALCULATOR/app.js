f2d1a897c80875514f2f3682e871221a32f6171e
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-09-23 23:29:43 +0900

    HTML, CSS, JavaScript 로 만든 계산기
    자바스크립트를 forEach 써서 만들었음

---
+++ CALCULATOR/app.js
@@ -0,0 +1,31 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn_clear");
+  let equal = document.querySelector(".btn_equal");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+  equal,
+    addEventListener("click", function (e) {
+      if (screen.value === "") {
+        screen.value === "";
+      } else {
+        let answer = eval(screen.value);
+        screen.value = answer;
+      }
+    });
+
+  clear.addEventListener("click", function (e) {
+    screen.value === "";
+  });
+})();
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   /*copy paste the already typed code here*/
+// });
