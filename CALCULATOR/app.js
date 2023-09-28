524cb45d25c57932f3e76dc84505b0b0c0b551fd
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-09-28 16:19:56 +0900

    = (equal) 클릭 오류 개선
    =(equal) 버튼 클릭 시 계산이 되지 않고 undefined 가 결과 창에 뜨는 오류가 있었음.
    nullish 연산자 (??) 사용해서 공백을 반환함으로써 해결하였음.

--- CALCULATOR/app.js
+++ CALCULATOR/app.js
@@ -1,31 +1,34 @@
-(function () {
-  let screen = document.querySelector(".screen");
-  let buttons = document.querySelectorAll(".btn");
-  let clear = document.querySelector(".btn_clear");
-  let equal = document.querySelector(".btn_equal");
+let screen = document.querySelector(".screen");
+let buttons = document.querySelectorAll(".btn");
+let clear = document.querySelector(".btn_clear");
+let equal = document.querySelector(".btn_equal");
 
-  buttons.forEach(function (button) {
-    button.addEventListener("click", function (e) {
-      let value = e.target.dataset.num;
-      screen.value += value;
-    });
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    let value = e.target.dataset.num ?? ""; // nullish 좌항이 null or unde인 경우 우항을 반환
+    screen.value += value;
+    console.log(value);
   });
+});
 
-  equal,
-    addEventListener("click", function (e) {
-      if (screen.value === "") {
-        screen.value === "";
-      } else {
-        let answer = eval(screen.value);
-        screen.value = answer;
-      }
-    });
-
-  clear.addEventListener("click", function (e) {
+equal.addEventListener("click", function (e) {
+  if (screen.value === "") {
     screen.value === "";
-  });
-})();
+  } else {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  }
+});
+
+clear.addEventListener("click", function (e) {
+  screen.value = "";
+});
 
 // document.addEventListener("DOMContentLoaded", function () {
 //   /*copy paste the already typed code here*/
 // });
+
+// "" || "?" 펄시 값이여서 우항으로 감
+// "" ?? "?" 널리쉬는 널이나 언디 일떄만 우항으로 감
+// 사용자 입력 값이 없으면 널이 뜸. 이 경우에 원하는 값으로 지정해주고 싶을 때 널리쉬 연산자 ?? 를 쓸 수 있음.
+// 옵셔널 체이닝 ?. 값이 없으면 언디파인드로 바꿔서 오류를 띄우지 않고 넘어감.
